749f6d2d885d7151b7fe6a2d3d68157a545710f6
Harshith <harshithprakashmutta@gmail.com>
2021-08-15 16:23:08 +0300

    Adds Income Expense Counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,8 @@ export class AppComponent {
   title = 'expense-tracker';
   transactions: Transaction[] = [];
   total: number = 0;
+  income = 0;
+  expense = 0;
 
   constructor(public transactionStore: TransactionStoreService) {
     const observer: PartialObserver<Transaction[]> = {
@@ -21,6 +23,13 @@ export class AppComponent {
         this.transactions = x.slice(Math.max(x.length - 3, 0)).reverse();
         const amounts = x.map((transaction) => transaction.amount);
         this.total = amounts.reduce((acc, item) => (acc += item), 0);
+        this.income = amounts
+          .filter((inc) => inc > 0)
+          .reduce((acc, item) => (acc += item), 0);
+        this.expense =
+          amounts
+            .filter((exp) => exp < 0)
+            .reduce((acc, item) => (acc += item), 0) * -1;
       },
       error: (err: Error) => console.error('Error: ' + err),
       complete: () => {},
