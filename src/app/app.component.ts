4b7da332091f2812dd81736d9b80eec9d364c986
Harshith <harshithprakashmutta@gmail.com>
2021-08-15 16:14:22 +0300

    Adds balance and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,14 @@ import {
 export class AppComponent {
   title = 'expense-tracker';
   transactions: Transaction[] = [];
+  total: number = 0;
 
   constructor(public transactionStore: TransactionStoreService) {
     const observer: PartialObserver<Transaction[]> = {
       next: (x: Transaction[]) => {
         this.transactions = x.slice(Math.max(x.length - 3, 0)).reverse();
+        const amounts = x.map((transaction) => transaction.amount);
+        this.total = amounts.reduce((acc, item) => (acc += item), 0);
       },
       error: (err: Error) => console.error('Error: ' + err),
       complete: () => {},
